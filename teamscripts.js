f1e81e8b8035d32912b3f4e37384ecf43ea8df63
Lou Guberti Ng <louguberti@gmail.com>
2025-10-31 17:16:16 +0800

    tidy up

--- teamscripts.js
+++ teamscripts.js
@@ -1,4 +1,3 @@
-console.log("entering the script file")
 const logos = {
     devils:`<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_33_1936)">
