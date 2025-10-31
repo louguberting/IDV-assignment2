7c1ddd46536f58f480439dde4b1cc7801516f326
Lou Guberti Ng <louguberti@gmail.com>
2025-10-31 16:28:27 +0800

    add d3 properlyer

--- teamscripts.js
+++ teamscripts.js
@@ -1,3 +1,5 @@
+import * as d3 from "d3";
+
 const logos = {
     devils:`<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_33_1936)">
